04314d8b5a498198dd07c8bbfcba9365cc92a285
Elena Martínez <elenams2000@hotmail.com>
2026-02-19 13:00:37 +0100

    qqqq

---
+++ create_superuser.py
@@ -0,0 +1,20 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+# Configura Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Shodo.settings")
+django.setup()
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "shodomarketingyeventos@gmail.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Raquelsanchez72.")
+
+if not User.objects.filter(username=username).exists():
+    print(f"Creando superusuario {username}...")
+    User.objects.create_superuser(username, email, password)
+    print("Superusuario creado exitosamente.")
+else:
+    print(f"El superusuario {username} ya existe.")
